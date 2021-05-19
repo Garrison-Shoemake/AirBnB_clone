c39b3665b2f17a29e408f100024975303ab0986f
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-19 01:09:33 +0000

    add new test in test file

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -10,11 +10,19 @@ from models.base_model import BaseModel
 class TestBase(unittest.TestCase):
     """ These tests will be conducted on the base_model class """
 
-    def test_save(self):
+    def test_self_id(self):
         """ this method tests BaseModel's save() method """
         b1 = BaseModel()
         b1.save()
         self.assertNotEqual(b1.created_at, b1.updated_at)
+
+    def test_save(self):
+        model = BaseModel()
+        time1 = model.updated_at
+        model.save()
+        time2 = model.updated_at
+        self.assertNotEqual(time1, time2)
+
 
 if __name__ == '__main__':
     unittest.main()
