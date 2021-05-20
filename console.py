227dd90df082c49774f706d8474b7d2fd8a1c0ce
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-20 19:18:44 +0000

    add console file

---
+++ console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" This is the Console file """
+
+
+import cmd
+import string, sys
+
+
+class HBNBCommand(cmd.Cmd):
+    """ this class defines the console class """
+
+    prompt = "(hbnb)"
+
+    def do_EOF(self, notself):
+        print()
+        return True
+
+
+    def do_quit(self):
+        """ Exits (hbnb) console """
+        sys.exit(1)
+
+    def emptyline(self):
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
