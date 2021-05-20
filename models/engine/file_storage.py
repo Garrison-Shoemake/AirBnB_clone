db495360587718ead137b32948e90b5622a976e7
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-20 19:19:50 +0000

    adding serialization management

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,22 +18,30 @@ class FileStorage:
 
     def new(self, obj):
         """sets obj with obj class.id"""
-        key = obj.__class__.__name__ + '.' + obj.id
+        name = obj.__class__.__name__i
+        key = name + '.' + obj.id
         self.__objects[key] = obj
 
     def save(self):
         """serialize __objects to json"""
         filename = self.__file_path
         json_dict = {}
-
+        # translate each instance to dictionary format so json can read.
         with open(filename, 'w') as objs_to_json:
-            json_dict = self.__objects
-            objs_to_json.write(json.dumps(json_dict, indent=2))
+            for key, value in self.__objects.items():
+                json_dict[key] = value.to_dict()
+            json.dump(json_dict, objs_to_json)
 
     def reload(self):
         """ deserialize the json file to objs"""
         filename = self.__file_path
 
-        if os.path.exists(filename):
-            with open(filename, 'r') as json_to_objs:
-                self.__objects = json.loads(json_to_objs)
+        # if os.path.exists(filename):
+        try:
+            with open(filename, 'r', encoding='utf-8') as json_to_objs:
+                tmp_dict = json.load(json_to_objs)
+        # loop through json file, update object at that key to the class args
+                for key, value in tmp_dict.items():
+                    self.__objects[key] = BaseModel(**value)
+        except:
+            pass
