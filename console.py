8cc2e14b8c9de0532666e86ec50c580d23f7377b
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-20 21:45:27 +0000

    add console progress to git

--- console.py
+++ console.py
@@ -4,7 +4,7 @@
 
 import cmd
 import string, sys
-
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """ this class defines the console class """
@@ -15,7 +15,6 @@ class HBNBCommand(cmd.Cmd):
         print()
         return True
 
-
     def do_quit(self, notself):
         """ Exits (hbnb) console """
         return True
@@ -23,6 +22,18 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         pass
 
+    def do_create(self, class_name):
+        if not class_name:
+            print("** class name missing **")
+            return
+        if class_name == 'BaseModel':
+            new_class = BaseModel()
+            new_class.save()
+            print(new_class.id)
+            return
+        else:
+            print("** class doesn't exist **")
+            return
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
