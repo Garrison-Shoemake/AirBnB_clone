c885ab153ab935eee56d4a648b2cbcad0caa0c91
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-22 23:48:15 +0000

    another one

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -16,17 +16,32 @@ class TestFileStorage(unittest.TestCase):
         cls.f = inspect.getmembers(FileStorage, inspect.isfunction)
 
     def test_save(self):
-        __file_path = "file.json"
+        f = FileStorage()
+        if os.path.exists(f._FileStorage__file_path):
+            os.remove(f._FileStorage__file_path)
+        b = BaseModel()
         storage.save()
-        self.assertTrue(os.path.exists("file.json"))
+        self.assertTrue(os.path.exists(f._FileStorage__file_path))
 
     def test_all(self):
         type_to_test = storage.all()
-        self.assertEqual(type(type_to_test), dict)
+        self.assertIsNotNone(type_to_test)
+        self.assertIsInstance(storage.all(), dict)
 
     def test_new(self):
         d = BaseModel()
         __objects = storage.new(d)
+
+    def test_reload(self):
+        f = FileStorage()
+        if os.path.exists(f._FileStorage__file_path):
+            os.remove(f._FileStorage__file_path)
+        f1 = FileStorage()
+        f2 = FileStorage()
+        f2.save()
+        f2.reload()
+        testreload = f2.all()
+        self.assertEqual(testreload, f1.all())
 
 if __name__ == '__main__':
     unittest.main()
